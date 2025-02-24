2436e805029a4adda9ed4f37df02c025465883f3
altescy <altescy@fastmail.com>
2025-02-24 15:46:42 +0900

    rename step.py -> workflow.py

--- formed/integrations/ml/__init__.py
+++ formed/integrations/ml/__init__.py
@@ -3,5 +3,5 @@ from .datamodule import DataModule, FieldConfig, extract_fields, set_datamodule,
 from .dataset import Dataset  # noqa: F401
 from .fields import *  # noqa: F401, F403
 from .indexers import Indexer, LabelIndexer, TokenIndexer  # noqa: F401
-from .steps import build_datamodule  # noqa: F401
 from .transforms import FieldTransform, LabelFieldTransform, ListFieldTransform, ScalarFieldTransform  # noqa: F401
+from .workflow import build_datamodule  # noqa: F401
