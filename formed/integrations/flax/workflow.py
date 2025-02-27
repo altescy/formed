68aa0302567049b9ace5ff00aa1ea2d22f90b838
altescy <altescy@fastmail.com>
2025-02-27 17:48:06 +0900

    accept hf dataset & val split

--- formed/integrations/flax/workflow.py
+++ formed/integrations/flax/workflow.py
@@ -6,11 +6,17 @@ from colt import Lazy
 from flax import nnx
 
 from formed.integrations.ml import DataModule, Dataset
+from formed.integrations.ml.workflow import split_dataset
 from formed.workflow import step, use_step_logger
 
 from .model import FlaxModel
 from .training import FlaxTrainer, TrainState
 from .types import DataT, ModelInputT, ModelOutputT, ModelParamsT
+
+try:
+    from formed.integrations.datasets.types import Dataset as HfDataset
+except ImportError:
+    HfDataset = None  # type: ignore[misc]
 
 T = TypeVar("T")
 
@@ -19,8 +25,8 @@ T = TypeVar("T")
 def train_flax_model(
     model: Lazy[FlaxModel[ModelInputT, ModelOutputT, ModelParamsT]],
     trainer: FlaxTrainer[DataT, ModelInputT, ModelOutputT, ModelParamsT],
-    train_dataset: Union[Sequence[T], Sequence[DataT]],
-    val_dataset: Optional[Union[Sequence[T], Sequence[DataT]]] = None,
+    train_dataset: Union[Sequence[T], Sequence[DataT], HfDataset],
+    val_dataset: Optional[Union[float, Sequence[T], Sequence[DataT], HfDataset]] = None,
     datamodule: Optional[DataModule[T]] = None,
     seed: int = 0,
 ) -> FlaxModel:
@@ -41,8 +47,21 @@ def train_flax_model(
         datamodule.build(train_dataset)
         train_dataset = cast(Sequence[DataT], Dataset.from_iterable(datamodule(train_dataset)))
         if val_dataset is not None:
-            val_dataset = cast(Sequence[T], val_dataset)
-            val_dataset = cast(Sequence[DataT], Dataset.from_iterable(datamodule(val_dataset)))
+            if isinstance(val_dataset, float):
+                if val_dataset < 0.0 or val_dataset > 1.0:
+                    raise ValueError("val_dataset must be in the range [0.0, 1.0]")
+                logger.info("Splitting dataset...")
+                splitted_datasets = split_dataset(
+                    train_dataset, {"train": 1.0 - val_dataset, "val": val_dataset}, seed=seed
+                )
+                logger.info(
+                    "Splitted dataset sizes: %s", {key: len(dataset) for key, dataset in splitted_datasets.items()}
+                )
+                train_dataset = splitted_datasets["train"]
+                val_dataset = splitted_datasets["val"]
+            else:
+                val_dataset = cast(Sequence[T], val_dataset)
+                val_dataset = cast(Sequence[DataT], Dataset.from_iterable(datamodule(val_dataset)))
 
     def train() -> TrainState:
         logger.info("Training model...")
