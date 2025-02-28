35389768e5fd5eb993aee3f95d1704ba70a7eb82
altescy <altescy@fastmail.com>
2025-02-28 16:54:19 +0900

    add predictor

--- examples/text_classification/text_classification.py
+++ examples/text_classification/text_classification.py
@@ -1,19 +1,32 @@
+import dataclasses
 import logging
-from collections.abc import Mapping
+import sys
+from collections.abc import Iterable, Iterator, Mapping
 from typing import Annotated, Optional, Union
+
+sys.path.append("../..")
 
 import datasets
 import jax
 import optax
+from collatable import Collator
 from flax import nnx, struct
 from rich.logging import RichHandler
 
+from formed.common.iterutils import batched
 from formed.integrations.flax import FlaxModel, FlaxTrainer
-from formed.integrations.ml import Dataset, FieldConfig, LabelFieldTransform, TextFieldTransform, use_datamodule
+from formed.integrations.flax.utils import numpy_to_jax
+from formed.integrations.ml import (
+    DataModule,
+    Dataset,
+    FieldConfig,
+    LabelFieldTransform,
+    TextFieldTransform,
+    use_datamodule,
+)
 
 logging.basicConfig(
     level="INFO",
-    datefmt="[%X]",
     handlers=[RichHandler()],
 )
 
@@ -28,8 +41,14 @@ class TextClassifier(
 ):
     @struct.dataclass
     class Input:
-        text: Annotated[Mapping[str, jax.Array], FieldConfig("text", TextFieldTransform)]
-        label: Annotated[Optional[jax.Array], FieldConfig("label", LabelFieldTransform)] = None
+        text: Annotated[
+            Mapping[str, jax.Array],
+            FieldConfig("text", TextFieldTransform(pad_token="<pad>", unk_token="<unk>")),
+        ]
+        label: Annotated[
+            Optional[jax.Array],
+            FieldConfig("label", LabelFieldTransform()),
+        ] = None
 
     @struct.dataclass
     class Output:
@@ -81,18 +100,56 @@ class TextClassifier(
         return self.Output(probs=probs, loss=loss, metrics=metrics)
 
 
+class Predictor:
+    @dataclasses.dataclass
+    class Prediction:
+        label: str
+        probability: float
+
+    def __init__(
+        self,
+        model: TextClassifier,
+        datamodule: DataModule,
+    ) -> None:
+        self._model = model
+        self._datamodule = datamodule
+        self._collator = Collator()
+
+    def __call__(self, dataset: Iterable) -> Iterator[Prediction]:
+        for batch in batched(self._datamodule(dataset), 32):
+            inputs = self._model.Input(**numpy_to_jax(self._collator(batch)))
+            output = self._model(inputs)
+
+            for label_index, probs in zip(output.probs.argmax(axis=-1), output.probs):
+                yield self.Prediction(
+                    label=self._datamodule.field("label").reconstruct(label_index),
+                    probability=probs[label_index].item(),
+                )
+
+
 if __name__ == "__main__":
+    import itertools
+
+    from flax import nnx
+
     train_dataset = datasets.load_dataset("stanfordnlp/imdb", split="train")
 
     datamodule = TextClassifier.default_data_module()
     assert datamodule is not None
 
     datamodule.build(train_dataset)
-    datamodule.activate()
 
     train_instances = Dataset.from_iterable(datamodule(train_dataset))
 
-    model = TextClassifier()
+    with datamodule:
+        model = TextClassifier()
 
-    trainer = FlaxTrainer(max_epochs=5, logging_strategy="step", logging_interval=100)  # type: ignore
-    trainer.train(nnx.Rngs(0), model, train_instances)
+        trainer = FlaxTrainer(max_epochs=10, logging_strategy="step", logging_interval=100)  # type: ignore
+        state = trainer.train(nnx.Rngs(0), model, train_instances)
+
+    model = nnx.merge(state.graphdef, state.params, *state.additional_states)
+
+    test_dataset = datasets.load_dataset("stanfordnlp/imdb", split="test[:10]")
+    predictor = Predictor(model, datamodule)
+    for prediction in predictor(test_dataset):
+        print(prediction)
