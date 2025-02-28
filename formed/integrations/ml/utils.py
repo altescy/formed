31cfdafe3cb43d822cbf7aa6e0c199fc88447e7a
altescy <altescy@fastmail.com>
2025-02-28 16:50:19 +0900

    modify regex tokenizer

--- formed/integrations/ml/utils.py
+++ formed/integrations/ml/utils.py
@@ -7,7 +7,9 @@ from collatable.utils import debatched  # noqa: F401
 
 
 class RegexTokenizer:
-    _DEFAULT_TOKENIZER_PATTERN: ClassVar[Pattern] = re.compile(r"[^\s.,!?:;/]+(?:[-']\[^\s.,!?:;/]+)*|[.,!?:;/]")
+    _DEFAULT_TOKENIZER_PATTERN: ClassVar[Pattern] = re.compile(
+        r"[^\s.,!?:;/\(\)]+(?:[-']\[^\s.,!?:;/\(\)]+)*|[.,!?:;/\(\)]"
+    )
 
     def __init__(self, pattern: Optional[Pattern] = None) -> None:
         self._token_pattern = pattern or self._DEFAULT_TOKENIZER_PATTERN
