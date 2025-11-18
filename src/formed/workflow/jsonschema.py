32c859f0d48b0073093c5cda028f1c830ce3d69f
altescy <me@altescy.jp>
2025-11-19 01:01:57 +0900

    fix json schema generation

--- src/formed/workflow/jsonschema.py
+++ src/formed/workflow/jsonschema.py
@@ -1,6 +1,7 @@
 from typing import Any, Final, Mapping, TypedDict
 
-from formed.common.jsonschema import generate_json_schema
+from colt import Registrable
+from formed.common.jsonschema import generate_json_schema, generate_definitions_from_registrable
 from formed.workflow import WorkflowStep
 
 from .constants import WORKFLOW_REFKEY, WORKFLOW_REFTYPE
@@ -16,7 +17,7 @@ _REF_SCHEMA_REF: Final[Mapping[str, Any]] = {"$ref": "#/$defs/__ref__"}
 
 
 def _ref_callback(path: str | None, schema: dict[str, Any]) -> dict[str, Any]:
-    if path and path.startswith("steps.") and "const" not in schema and _REF_SCHEMA_REF not in schema.get("anyOf", []):
+    if "const" not in schema and _REF_SCHEMA_REF not in schema.get("anyOf", []):
         return {"anyOf": [schema, _REF_SCHEMA_REF]}
     return schema
 
@@ -25,9 +26,17 @@ def generate_workflow_schema(
     title: str = "Formed Workflow Graph",
 ) -> dict[str, Any]:
     WorkflowGraphSchema = TypedDict("WorkflowGraphSchema", {"steps": dict[str, WorkflowStep]})
+    definitions = {"__ref__": _REF_SCHEMA}
+    for subclass in Registrable.__subclasses__():
+        definitions.update(
+            generate_definitions_from_registrable(
+                subclass,
+                callback=_ref_callback,
+            )
+        )
     return generate_json_schema(
         WorkflowGraphSchema,
-        definitions={"__ref__": _REF_SCHEMA},
+        definitions=definitions,
         callback=_ref_callback,
         title=title,
     )
