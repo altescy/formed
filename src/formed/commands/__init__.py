9c00eab41099166fd8ab1986c69a5f9833de27f3
altescy <me@altescy.jp>
2025-12-20 20:52:10 +0900

    load settings before load commands

--- src/formed/commands/__init__.py
+++ src/formed/commands/__init__.py
@@ -1,8 +1,15 @@
 import argparse
 
 from formed import __version__
-from formed.commands import workflow  # noqa: F401
+from formed.commands import workflow
 from formed.commands.subcommand import Subcommand
+from formed.settings import load_formed_settings
+
+__all__ = [
+    "create_subcommand",
+    "main",
+    "workflow",
+]
 
 
 def create_subcommand(prog: str | None = None) -> Subcommand:
@@ -16,5 +23,6 @@ def create_subcommand(prog: str | None = None) -> Subcommand:
 
 
 def main(prog: str | None = None) -> None:
+    load_formed_settings(None)
     app = create_subcommand(prog)
     app()
